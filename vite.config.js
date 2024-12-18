13900b4d0b16b8aff1e98d68f30f7ccc83e2301f
yhsdaniel <user36@realtechltd.com>
2024-12-18 17:11:20 +0700

    update vercel json

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+    sourcemap: false
+  },
+  base: '/'
 })
